6fe9aed58d90dad67020d0df81eb149b9a031b90
Mark Drobnak <mdrobnak@mozilla.com>
2020-06-05 16:54:05 -0400

    Add extractors to webpush_route

--- autoendpoint/src/server/routes/webpush.rs
+++ autoendpoint/src/server/routes/webpush.rs
@@ -1,6 +1,8 @@
+use crate::server::extractors::notification::Notification;
+use crate::server::extractors::subscription::Subscription;
 use actix_web::HttpResponse;
 
 /// Handle the `/wpush/{api_version}/{token}` and `/wpush/{token}` routes
-pub async fn webpush_route() -> HttpResponse {
+pub async fn webpush_route(notification: Notification, subscription: Subscription) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
