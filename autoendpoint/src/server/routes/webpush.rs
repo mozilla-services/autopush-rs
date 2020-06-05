a9104b0e08d84df49da0e0edc6b55547c2f169ed
Mark Drobnak <mdrobnak@mozilla.com>
2020-06-05 11:01:22 -0400

    Add basic webpush extraction structs

--- autoendpoint/src/server/routes/webpush.rs
+++ autoendpoint/src/server/routes/webpush.rs
@@ -4,3 +4,27 @@ use actix_web::HttpResponse;
 pub async fn webpush_route() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
+
+pub struct TokenInfo {
+    api_version: String,
+    token: String,
+    crypto_key: String,
+    auth_header: String,
+}
+
+pub struct Subscription {
+    uaid: String,
+    channel_id: String,
+    version: String,
+    public_key: String,
+}
+
+pub struct Notification {
+    channel_id: String,
+    version: String,
+    ttl: String,
+    topic: String,
+    timestamp: String,
+    data: String,
+    headers: String,
+}
