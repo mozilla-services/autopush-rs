f949cf73a294749b4cc701ee1738b51bd574569e
Mark Drobnak <mdrobnak@mozilla.com>
2020-06-11 16:22:52 -0400

    Support passing crypto keys via environment variables
    Vecs are not supported for environment variables, so we have to accept a
    string and manually parse it.

--- autoendpoint/src/settings.rs
+++ autoendpoint/src/settings.rs
@@ -20,7 +20,7 @@ pub struct Settings {
     pub database_use_test_transactions: bool,
 
     pub max_data_bytes: usize,
-    pub crypto_keys: Vec<String>,
+    pub crypto_keys: String,
     pub human_logs: bool,
 
     pub statsd_host: Option<String>,
@@ -39,7 +39,7 @@ impl Default for Settings {
             #[cfg(any(test, feature = "db_test"))]
             database_use_test_transactions: false,
             max_data_bytes: 4096,
-            crypto_keys: vec![Fernet::generate_key()],
+            crypto_keys: format!("[{}]", Fernet::generate_key()),
             human_logs: false,
             statsd_host: None,
             statsd_port: 8125,
@@ -91,9 +91,13 @@ impl Settings {
 
     /// Initialize the fernet encryption instance
     pub fn make_fernet(&self) -> MultiFernet {
-        let fernets = self
-            .crypto_keys
-            .iter()
+        if !(self.crypto_keys.starts_with('[') && self.crypto_keys.ends_with(']')) {
+            panic!("Invalid AUTOEND_CRYPTO_KEY");
+        }
+
+        let crypto_keys = &self.crypto_keys[1..self.crypto_keys.len() - 1];
+        let fernets = crypto_keys
+            .split(',')
             .map(|key| Fernet::new(key).expect("Invalid AUTOEND_CRYPTO_KEY"))
             .collect();
         MultiFernet::new(fernets)
