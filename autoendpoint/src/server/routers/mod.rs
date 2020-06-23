bcf9bc06e24a3e3533dee872676d60da05e908d4
Mark Drobnak <mdrobnak@mozilla.com>
2020-06-23 14:29:24 -0400

    Use async_trait in Router and add RouterResponse

--- autoendpoint/src/server/routers/mod.rs
+++ autoendpoint/src/server/routers/mod.rs
@@ -2,10 +2,34 @@
 
 use crate::error::ApiResult;
 use crate::server::extractors::notification::Notification;
+use actix_web::http::StatusCode;
+use actix_web::HttpResponse;
+use async_trait::async_trait;
+use std::collections::HashMap;
 
 pub mod webpush;
 
+#[async_trait]
 pub trait Router {
     /// Route a notification to the user
-    fn route_notification(&self, notification: Notification) -> ApiResult<()>;
+    async fn route_notification(&self, notification: Notification) -> ApiResult<RouterResponse>;
+}
+
+/// The response returned when a router routes a notification
+pub struct RouterResponse {
+    pub status: StatusCode,
+    pub headers: HashMap<&'static str, String>,
+    pub body: Option<String>,
+}
+
+impl From<RouterResponse> for HttpResponse {
+    fn from(router_response: RouterResponse) -> Self {
+        let mut builder = HttpResponse::build(router_response.status);
+
+        for (key, value) in router_response.headers {
+            builder.set_header(key, value);
+        }
+
+        builder.body(router_response.body.unwrap_or_default())
+    }
 }
