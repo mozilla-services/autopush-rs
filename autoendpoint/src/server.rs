35d6f8a206db3d7de9a99eaa51ddbb62d0ddde89
Mark Drobnak <mdrobnak@mozilla.com>
2020-08-04 11:46:12 -0400

    Hook up the ADM router to the server

--- autoendpoint/src/server.rs
+++ autoendpoint/src/server.rs
@@ -4,6 +4,7 @@ use crate::db::client::{DbClient, DbClientImpl};
 use crate::error::{ApiError, ApiResult};
 use crate::metrics;
 use crate::middleware::sentry::sentry_middleware;
+use crate::routers::adm::router::AdmRouter;
 use crate::routers::apns::router::ApnsRouter;
 use crate::routers::fcm::router::FcmRouter;
 use crate::routes::health::{health_route, lb_heartbeat_route, status_route, version_route};
@@ -32,6 +33,7 @@ pub struct ServerState {
     pub http: reqwest::Client,
     pub fcm_router: Arc<FcmRouter>,
     pub apns_router: Arc<ApnsRouter>,
+    pub adm_router: Arc<AdmRouter>,
 }
 
 pub struct Server;
@@ -66,6 +68,13 @@ impl Server {
             )
             .await?,
         );
+        let adm_router = Arc::new(AdmRouter::new(
+            settings.adm.clone(),
+            settings.endpoint_url.clone(),
+            http.clone(),
+            metrics.clone(),
+            ddb.clone(),
+        )?);
         let state = ServerState {
             metrics,
             settings,
@@ -74,6 +83,7 @@ impl Server {
             http,
             fcm_router,
             apns_router,
+            adm_router,
         };
 
         let server = HttpServer::new(move || {
--- autoendpoint/src/settings.rs
+++ autoendpoint/src/settings.rs
@@ -1,5 +1,6 @@
 //! Application settings
 
+use crate::routers::adm::settings::AdmSettings;
 use crate::routers::apns::settings::ApnsSettings;
 use crate::routers::fcm::settings::FcmSettings;
 use config::{Config, ConfigError, Environment, File};
@@ -33,6 +34,7 @@ pub struct Settings {
 
     pub fcm: FcmSettings,
     pub apns: ApnsSettings,
+    pub adm: AdmSettings,
 }
 
 impl Default for Settings {
@@ -53,6 +55,7 @@ impl Default for Settings {
             statsd_label: "autoendpoint".to_string(),
             fcm: FcmSettings::default(),
             apns: ApnsSettings::default(),
+            adm: AdmSettings::default(),
         }
     }
 }
