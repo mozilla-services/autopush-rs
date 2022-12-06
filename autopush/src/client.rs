246e9a4c95c9495f3351b9fe1db1cb3e31951ea8
jrconlin <jr+git@mozilla.com>
2022-12-06 14:47:33 -0800

    f fix for SYNC-3516
    * Add metrics to track delivery to OS platforms
    * refactored UA parse to be a struct
    * some modern clippy cleanup

--- autopush/src/client.rs
+++ autopush/src/client.rs
@@ -27,7 +27,7 @@ use autopush_common::util::{ms_since_epoch, sec_since_epoch};
 use crate::megaphone::{Broadcast, BroadcastSubs};
 use crate::server::protocol::{ClientMessage, ServerMessage, ServerNotification};
 use crate::server::Server;
-use crate::user_agent::parse_user_agent;
+use crate::user_agent::UserAgentInfo;
 
 // Created and handed to the AutopushServer
 pub struct RegisteredClient {
@@ -163,6 +163,7 @@ pub struct WebPushClient {
     last_ping: u64,
     stats: SessionStatistics,
     deferred_user_registration: Option<DynamoDbUser>,
+    ua_info: UserAgentInfo,
 }
 
 impl Default for WebPushClient {
@@ -182,6 +183,7 @@ impl Default for WebPushClient {
             last_ping: Default::default(),
             stats: Default::default(),
             deferred_user_registration: Default::default(),
+            ua_info: Default::default(),
         }
     }
 }
@@ -473,6 +475,7 @@ where
                 ..Default::default()
             },
             deferred_user_registration,
+            ua_info: UserAgentInfo::from(user_agent.as_ref()),
             ..Default::default()
         }));
 
@@ -600,16 +603,17 @@ where
         }
         let now = ms_since_epoch();
         let elapsed = (now - webpush.connected_at) / 1_000;
-        let (ua_result, metrics_os, metrics_browser) = parse_user_agent(&user_agent);
+        let ua_info = UserAgentInfo::from(user_agent.as_ref());
         // dogstatsd doesn't support timers: use histogram instead
         srv.metrics
             .time_with_tags("ua.connection.lifespan", elapsed)
-            .with_tag("ua_os_family", metrics_os)
-            .with_tag("ua_browser_family", metrics_browser)
+            .with_tag("ua_os_family", &ua_info.metrics_os)
+            .with_tag("ua_browser_family", &ua_info.metrics_browser)
             .send();
 
         // Log out the sentry message if applicable and convert to error msg
         let error = if let Some(ref err) = error {
+            let ua_info = ua_info.clone();
             let mut event = event_from_error_chain(err);
             event.user = Some(sentry::User {
                 id: Some(webpush.uaid.as_simple().to_string()),
@@ -617,19 +621,19 @@ where
             });
             event
                 .tags
-                .insert("ua_name".to_string(), ua_result.name.to_string());
+                .insert("ua_name".to_string(), ua_info.browser_name);
             event
                 .tags
-                .insert("ua_os_family".to_string(), metrics_os.to_string());
+                .insert("ua_os_family".to_string(), ua_info.metrics_os);
             event
                 .tags
-                .insert("ua_os_ver".to_string(), ua_result.os_version.to_string());
+                .insert("ua_os_ver".to_string(), ua_info.os_version);
             event
                 .tags
-                .insert("ua_browser_family".to_string(), metrics_browser.to_string());
+                .insert("ua_browser_family".to_string(), ua_info.metrics_browser);
             event
                 .tags
-                .insert("ua_browser_ver".to_string(), ua_result.version.to_string());
+                .insert("ua_browser_ver".to_string(), ua_info.browser_version);
             sentry::capture_event(event);
             err.display_chain().to_string()
         } else {
@@ -658,12 +662,12 @@ where
         "uaid_reset" => stats.uaid_reset,
         "existing_uaid" => stats.existing_uaid,
         "connection_type" => &stats.connection_type,
-        "ua_name" => ua_result.name,
-        "ua_os_family" => metrics_os,
-        "ua_os_ver" => ua_result.os_version.into_owned(),
-        "ua_browser_family" => metrics_browser,
-        "ua_browser_ver" => ua_result.version,
-        "ua_category" => ua_result.category,
+        "ua_name" => ua_info.browser_name,
+        "ua_os_family" => ua_info.metrics_os,
+        "ua_os_ver" => ua_info.os_version,
+        "ua_browser_family" => ua_info.metrics_browser,
+        "ua_browser_ver" => ua_info.browser_version,
+        "ua_category" => ua_info.category,
         "connection_time" => elapsed,
         "direct_acked" => stats.direct_acked,
         "direct_storage" => stats.direct_storage,
@@ -1100,7 +1104,7 @@ where
                     webpush.unacked_direct_notifs.push(notif.clone());
                 }
                 debug!("Got a notification to send, sending!");
-                emit_metrics_for_send(&data.srv.metrics, &notif, "Direct");
+                emit_metrics_for_send(&data.srv.metrics, &notif, "Direct", &webpush.ua_info);
                 transition!(Send {
                     smessages: vec![ServerMessage::Notification(notif)],
                     data,
@@ -1217,7 +1221,9 @@ where
                 .extend(messages.iter().cloned());
             let smessages: Vec<_> = messages
                 .into_iter()
-                .inspect(|msg| emit_metrics_for_send(&data.srv.metrics, msg, "Stored"))
+                .inspect(|msg| {
+                    emit_metrics_for_send(&data.srv.metrics, msg, "Stored", &webpush.ua_info)
+                })
                 .map(ServerMessage::Notification)
                 .collect();
             webpush.sent_from_storage += smessages.len() as u32;
@@ -1337,11 +1343,17 @@ where
     }
 }
 
-fn emit_metrics_for_send(metrics: &StatsdClient, notif: &Notification, source: &'static str) {
+fn emit_metrics_for_send(
+    metrics: &StatsdClient,
+    notif: &Notification,
+    source: &'static str,
+    user_agent_info: &UserAgentInfo,
+) {
     metrics
         .incr_with_tags("ua.notification.sent")
         .with_tag("source", source)
         .with_tag("topic", &notif.topic.is_some().to_string())
+        .with_tag("os", &user_agent_info.metrics_os)
         // TODO: include `internal` if meta is set
         .send();
     metrics
@@ -1350,5 +1362,6 @@ fn emit_metrics_for_send(metrics: &StatsdClient, notif: &Notification, source: &
             notif.data.as_ref().map_or(0, |data| data.len() as i64),
         )
         .with_tag("source", source)
+        .with_tag("os", &user_agent_info.metrics_os)
         .send();
 }
