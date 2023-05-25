1489958a8e89c0062324ec23f9ac94033e67d1c4
Philip Jenvey <pjenvey@underboss.org>
2023-05-24 17:43:46 -0700

    f rewrite the broadcast unit test as an integration

--- autoconnect/autoconnect-ws/src/test.rs
+++ autoconnect/autoconnect-ws/src/test.rs
@@ -2,10 +2,9 @@ use std::{sync::Arc, time::Duration};
 
 use async_stream::stream;
 use futures::pin_mut;
-use serde_json::json;
 
 use autoconnect_common::{
-    protocol::{BroadcastValue, ServerMessage},
+    protocol::ServerMessage,
     test_support::{hello_db, HELLO, UA},
 };
 use autoconnect_settings::{AppState, Settings};
@@ -136,69 +135,4 @@ async fn auto_ping_timeout_after_pong() {
     pin_mut!(s);
     let err = webpush_ws(client, &mut session, s).await.unwrap_err();
     assert!(matches!(err, WSError::PongTimeout));
-}
-
-#[actix_web::test]
-async fn broadcast() {
-    let settings = Settings {
-        auto_ping_interval: Duration::from_secs_f32(0.15),
-        auto_ping_timeout: Duration::from_secs_f32(0.15),
-        ..Default::default()
-    };
-    let app_state = Arc::new(AppState {
-        db: hello_db().into_boxed_arc(),
-        ..AppState::from_settings(settings).unwrap()
-    });
-    app_state
-        .broadcaster
-        .write()
-        .await
-        .add_broadcast(("foo/bar".to_owned(), "v1".to_owned()).into());
-    let client = UnidentifiedClient::new(UA.to_owned(), Arc::clone(&app_state));
-
-    let mut seq = mockall::Sequence::new();
-    let mut session = MockSession::new();
-    session
-        .expect_text()
-        .times(1)
-        .in_sequence(&mut seq)
-        .withf(|msg| matches!(msg, ServerMessage::Hello { .. }))
-        .return_once(|_| Ok(()));
-    session
-        .expect_ping()
-        .times(1)
-        .in_sequence(&mut seq)
-        .return_once(|_| Ok(()));
-    session
-        .expect_text()
-        .times(1)
-        .in_sequence(&mut seq)
-        .withf(|smsg| {
-            matches!(
-                smsg,
-                ServerMessage::Broadcast { broadcasts }
-                if broadcasts.get("foo/bar") == Some(&BroadcastValue::Value("v2".to_owned())),
-            )
-        })
-        .return_once(|_| Ok(()));
-
-    let hello = json!({"messageType": "hello", "use_webpush": true,
-                       "broadcasts": {"foo/bar": "v1"}});
-    let s = stream! {
-        yield Ok(actix_ws::Message::Text(hello.to_string().into()));
-        // Wait for a Ping
-        tokio::time::sleep(Duration::from_secs_f32(0.2)).await;
-        app_state
-            .broadcaster
-            .write()
-            .await
-            .add_broadcast(("foo/bar".to_owned(), "v2".to_owned()).into());
-        yield Ok(actix_ws::Message::Pong("".into()));
-        // Wait for a Broadcast
-        tokio::time::sleep(Duration::from_secs_f32(0.2)).await;
-    };
-    pin_mut!(s);
-    webpush_ws(client, &mut session, s)
-        .await
-        .expect("Handler failed");
 }
