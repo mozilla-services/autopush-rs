f1bd9b9f5197413ab37a375f2852e56c77b51ad6
Benjamin Forehand <bennyjr169@gmail.com>
2023-08-31 18:22:46 -0500

    feat: Add send notification task to locust load test file
    This adds a send notification task to locust. It also updates the register task to handle errors a bit better.
    
    SYNC-3846
    
    Note: I didn't add the unregister function as it seems like we don't need that currently, but it can be added if needed.

--- tests/load/locustfiles/locustfile.py
+++ tests/load/locustfiles/locustfile.py
@@ -4,13 +4,17 @@
 
 """Performance test module."""
 
+import base64
 import json
+import random
+import ssl
+import string
 import time
 import uuid
 from typing import Any
 
 from locust import FastHttpUser, between, events, task
-from locust.exception import StopUser
+from locust.exception import RescheduleTask, StopUser
 from models import HelloMessage, RegisterMessage
 from websocket import create_connection
 
@@ -73,6 +77,17 @@ class AutopushUser(FastHttpUser):
         self.uaid: str = ""
         self.channels: dict[str, str] = {}
         self.ws: Any = None
+        self.headers = {"TTL": "60", "Content-Encoding": "aes128gcm", "Topic": "aaaa"}
+        self.encrypted_data = base64.urlsafe_b64decode(
+            "TestData"
+            + "".join(
+                [
+                    random.choice(string.ascii_letters + string.digits)
+                    for i in range(0, random.randrange(1024, 4096, 2) - 8)
+                ]
+            )
+            + "=="
+        )
 
     def on_start(self) -> Any:
         self.connect()
@@ -100,7 +115,8 @@ class AutopushUser(FastHttpUser):
         )
 
     def disconnect(self) -> None:
-        self.ws.close()
+        if self.ws:
+            self.ws.close()
 
     def hello(self) -> None:
         """
@@ -123,6 +139,31 @@ class AutopushUser(FastHttpUser):
             timer.response_length = len(reply.encode("utf-8"))
         self.uaid = res.uaid
 
+    def ack(self) -> None:
+        """
+        Send an 'ack' message to push.
+
+        After sending a notification, the client must also send an 'ack' to the server to
+        confirm receipt. If there is a pending notification, this will try and receive it
+        before sending an acknowledgement.
+        """
+        with self._time_event(name="acknowledge"):
+            channel_id = list(self.channels.keys())[-1]
+            body = json.dumps(
+                dict(messageType="ack", use_webpush=True, updates=dict(channelID=channel_id))
+            )
+
+            self.connect()
+            self.hello()
+
+            try:
+                self.ws.send(body)
+                self.ws.recv()
+            except ssl.SSLError:
+                self.ws.recv()
+                self.ws.send(body)
+
+    @task(weight=3)
     def register(self) -> None:
         """
         Send a 'register' message to Autopush. Subscribes to an Autopush channel.
@@ -131,19 +172,56 @@ class AutopushUser(FastHttpUser):
             AssertionError: If the user fails to register a channel
             ValidationError: If the register message schema is not as expected
         """
+        if not self.uaid:
+            raise RescheduleTask()
+
         chid: str = str(uuid.uuid4())
+        control: bool = True
 
-        with self._time_event(name="register") as timer:
-            body = json.dumps(dict(messageType="register", channelID=chid))
-            self.ws.send(body)
-            reply = self.ws.recv()
-            assert reply, f"No 'register' response CHID: {chid}"
-            res: RegisterMessage = RegisterMessage(**json.loads(reply))
-            assert res.status == 200, f"Unexpected status. Expected: 200 Actual: {res.status}"
-            assert res.channelID == chid, f"Channel ID did not match, received {res.channelID}"
-            timer.response_length = len(reply.encode("utf-8"))
-        self.channels[chid] = res.pushEndpoint
+        with self._time_event(name="send_notification") as timer:
+            while control:
+                try:
+                    body = json.dumps(dict(messageType="register", channelID=chid))
+                    self.ws.send(body)
+                    reply = self.ws.recv()
+                except ssl.SSLError:
+                    # if there is an error, disconnect and retry
+                    self.disconnect()
+                    self.connect()
+                    self.hello()
+                else:
+                    res: RegisterMessage = RegisterMessage(**json.loads(reply))
+                    assert (
+                        res.status == 200
+                    ), f"Unexpected status. Expected: 200 Actual: {res.status}"
+                    assert (
+                        res.channelID == chid
+                    ), f"Channel ID did not match, received {res.channelID}"
+                    timer.response_length = len(reply.encode("utf-8"))
+                    self.channels[chid] = res.pushEndpoint
+                    control = False
 
-    @task
-    def do_nothing(self) -> None:
-        pass
+    @task(weight=95)
+    def send_notification(self):
+        """
+        Sends a notification to a registered enpoint
+
+        Raises:
+            AssertionError: If the server does not respond correctly (400, 500, etc)
+        """
+        if not self.channels:
+            raise RescheduleTask()
+
+        self.disconnect()
+
+        with self._time_event(name="send_notification") as timer:
+            endpoint_url = random.choice(list(self.channels.items()))[-1]
+            endpoint_res = self.client.post(
+                url=endpoint_url,
+                name="Endpoint Notification",
+                data=self.encrypted_data,
+                headers=self.headers,
+            )
+            assert endpoint_res.status_code == 201, f"status code was {endpoint_res.status_code}"
+            timer.response_length = len(endpoint_res.text.encode("utf-8"))
+        self.ack()
