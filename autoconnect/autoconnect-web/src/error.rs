1539604a8a679dcb09a991b60d918ae43ff2c46a
Philip Jenvey <pjenvey@underboss.org>
2023-09-05 15:31:24 -0700

    feat: emit a tag in autoconnect's metrics
    to distinguish its metrics from the legacy version
    
    and consolidate autoendpoint's metrics builder w/ autopush_common's
    
    SYNC-3893

--- autoconnect/autoconnect-web/src/error.rs
+++ autoconnect/autoconnect-web/src/error.rs
@@ -24,8 +24,9 @@ impl ResponseError for ApiError {
     }
 
     fn error_response(&self) -> HttpResponse {
-        HttpResponse::build(self.status_code()).json(json!({
-            "code": self.status_code().as_u16(),
+        let code = self.status_code();
+        HttpResponse::build(code).json(json!({
+            "code": code.as_u16(),
             "errno": self.errno(),
             "error": self.to_string(),
         }))
