d01e19dfece58d58c722e3adf80216fd4beeb966
Katrina Anderson <3422688+Trinaa@users.noreply.github.com>
2023-12-13 11:22:07 -0500

    fix: LOCUST_HOST value in setup_k8s.sh

--- tests/load/setup_k8s.sh
+++ tests/load/setup_k8s.sh
@@ -28,7 +28,7 @@ echo "Image tag for locust is set to: ${LOCUST_IMAGE_TAG}"
 ENVIRONMENT_VARIABLES=(
   "TARGET_HOST,$TARGET"
   'LOCUST_CSV,autopush'
-  'LOCUST_HOST,wss://autopush.stage.mozaws.net'
+  'LOCUST_HOST,wss://autoconnect.stage.mozaws.net'
   'LOCUST_LOGLEVEL,INFO'
   'LOCUST_LOGFILE,autopush.log'
 )
