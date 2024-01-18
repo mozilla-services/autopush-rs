5e2cdf690dc1567b7f9083f254ed25539b241662
jrconlin <jr+git@mozilla.com>
2024-01-17 17:09:31 -0800

    feat: Be more clear and consistent about features around `dual`
    This patch makes the feature sets consistent between the major crates.
    
    Closes SYNC-4075

--- autopush-common/src/db/mod.rs
+++ autopush-common/src/db/mod.rs
@@ -61,9 +61,13 @@ pub enum StorageType {
 impl StorageType {
     fn available<'a>() -> Vec<&'a str> {
         #[allow(unused_mut)]
-        let mut result = ["DynamoDB"].to_vec();
+        let mut result: Vec<&str> = Vec::new();
+        #[cfg(feature = "dynamodb")]
+        result.push("DynamoDB");
         #[cfg(feature = "bigtable")]
         result.push("Bigtable");
+        #[cfg(all(feature = "bigtable", feature = "dynamodb"))]
+        result.push("Dual");
         result
     }
 
