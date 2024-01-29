b8233d0865579561a3e5e5cf95dfa43196d37ae7
jrconlin <jr+git@mozilla.com>
2024-01-29 10:09:46 -0800

    f remove `dual_test.sh` from repo

--- dual_test.sh
+++
@@ -1,4 +0,0 @@
-DB_DSN=dual \
-    DB_SETTINGS="{\"primary\":{\"db_settings\":\"{\\\"message_family\\\":\\\"message\\\",\\\"router_family\\\":\\\"router\\\",\\\"table_name\\\":\\\"projects/test/instances/test/tables/test\\\"}\",\"dsn\":\"grpc://bigtable.googleapis.com\"},\"secondary\":{\"db_settings\":\"{\\\"message_table\\\":\\\"test_message\\\",\\\"router_table\\\":\\\"test_router\\\"}\",\"dsn\":\"http://localhost:8000/\"}}" \
-    TEST_RESULTS_DIR=tmp \
-    make integration-test
