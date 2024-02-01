6fcb67b69d1a44678ef921a56a693c97e1d340b8
jrconlin <jr+git@mozilla.com>
2024-02-01 10:48:22 -0800

    f flake8

--- tests/load/locustfiles/load.py
+++ tests/load/locustfiles/load.py
@@ -48,8 +48,13 @@ class AutopushLoadTestShape(LoadTestShape):
     """
 
     MAX_RUN_TIME: int = os.environ.get("MAX_RUN_TIME", 600)  # 10 minutes
-    WORKER_COUNT: int = os.environ.get("WORKER_COUNT", 300)  # Must match value defined in setup_k8s.sh
-    USERS_PER_WORKER: int = os.environ.get("USERS_PER_WORKER", 500)  # Number of users supported on a worker running on a n1-standard-2
+
+    # Must match value defined in setup_k8s.sh
+    WORKER_COUNT: int = os.environ.get("WORKER_COUNT", 300)
+
+    # Number of users supported on a worker running on a n1-standard-2
+    USERS_PER_WORKER: int = os.environ.get("USERS_PER_WORKER", 500)
+
     MAX_USERS: int = WORKER_COUNT * USERS_PER_WORKER
     trend: QuadraticTrend
     user_classes: list[Type[User]] = [AutopushUser]
