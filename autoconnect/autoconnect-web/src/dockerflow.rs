6338f6ddc180cf8a9c0181fe2db32229beacf019
jrconlin <jr+git@mozilla.com>
2024-02-08 12:29:42 -0800

    bug: Add errors for health check reporting
    (NOTE: This uses the `error!()` macro.)

--- autoconnect/autoconnect-web/src/dockerflow.rs
+++ autoconnect/autoconnect-web/src/dockerflow.rs
@@ -27,7 +27,16 @@ pub fn config(config: &mut web::ServiceConfig) {
 
 /// Handle the `/health` and `/__heartbeat__` routes
 pub async fn health_route(state: Data<AppState>) -> Json<serde_json::Value> {
-    let status = if state.db.health_check().await.is_ok() {
+    let status = if state
+        .db
+        .health_check()
+        .await
+        .map_err(|e| {
+            error!("Autoconnect Health Error: {:?}", e);
+            e
+        })
+        .is_ok()
+    {
         "OK"
     } else {
         "ERROR"
