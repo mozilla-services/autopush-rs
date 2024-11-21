03d92984ccb7e39aae95d0ff618b5ca50f777fad
jrconlin <jr+git@mozilla.com>
2024-11-21 10:14:15 -0800

    f r's

--- autopush-common/src/lib.rs
+++ autopush-common/src/lib.rs
@@ -44,10 +44,10 @@ pub mod util;
 /// "abandoned" and any router info assigned to a User Agent that has not contacted
 /// Autopush in 60 days can be discarded.
 ///
-const ONE_DAY: u64 = 24 * 60 * 60;
+const ONE_DAY_IN_SECONDS: u64 = 24 * 60 * 60;
 /// The maximum TTL for notifications, 30 days in seconds
-pub const MAX_NOTIFICATION_TTL: u64 = 30 * ONE_DAY;
+pub const MAX_NOTIFICATION_TTL: u64 = 30 * ONE_DAY_IN_SECONDS;
 /// FCM has a max TTL of 4 weeks.
-pub const MAX_FCM_NOTIFICATION_TTL: u64 = 4 * 7 * ONE_DAY;
+pub const MAX_FCM_NOTIFICATION_TTL: u64 = 4 * 7 * ONE_DAY_IN_SECONDS;
 /// The maximum TTL for router records, 60 days in seconds
 pub const MAX_ROUTER_TTL: u64 = 2 * MAX_NOTIFICATION_TTL;
