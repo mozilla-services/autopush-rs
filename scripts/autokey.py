101c7fbb1b55c0d815ac51377dde45d879862b78
JR Conlin <jr+git@mozilla.com>
2026-03-02 15:04:37 -0800

    feat: Restore `autokey` key generation script (#1085)
    * add convenience function to gen the keys
    * add option cross reference to the `running.md` file
    
    Closes #PUSH-651

---
+++ scripts/autokey.py
@@ -0,0 +1,16 @@
+"""A command-line utility that generates endpoint encryption keys."""
+from cryptography.fernet import Fernet
+
+
+def main():
+    key = Fernet.generate_key().decode("utf-8")
+    print("\n# Crypto Keys:")
+    print(f"AUTOCONNECT__CRYPTO_KEY=[\"{key}\"]")
+    print(f"AUTOEND__CRYPTO_KEYS=[\"{key}\"]")
+    print("\n\n# Auth Key:")
+    key = Fernet.generate_key().decode("utf-8")
+    print(f"AUTOEND__AUTH_KEYS=[\"{key}\"]")
+
+
+if __name__ == "__main__":
+    main()
